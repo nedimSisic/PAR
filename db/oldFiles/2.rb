ea796b2ba93930a9e17a95c9605fffb8cccf2bdd
Nedim Sisic <nsisic1@etf.unsa.ba>
2022-05-27 02:10:39 +0200

    Delete and create database

--- db/oldFiles/2.rb
+++ db/oldFiles/2.rb
@@ -1,3 +1,5 @@
+20220523194955_add_password_column_to_users.rb
+
 class AddPasswordColumnToUsers < ActiveRecord::Migration[7.0]
   def change
     add_column :users, :password_digest, :string
--- db/oldFiles/sch.rb
+++ db/oldFiles/sch.rb
@@ -1,3 +1,5 @@
+schema.rb
+
 # This file is auto-generated from the current state of the database. Instead
 # of editing this file, please use the migrations feature of Active Record to
 # incrementally modify your database, and then regenerate this schema definition.
@@ -10,5 +12,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 0) do
+ActiveRecord::Schema[7.0].define(version: 2022_05_23_194955) do
+  create_table "users", force: :cascade do |t|
+    t.string "username", null: false
+    t.string "email", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.string "password_digest"
+    t.index ["email"], name: "index_users_on_email", unique: true
+    t.index ["username"], name: "index_users_on_username", unique: true
+  end
+
 end
