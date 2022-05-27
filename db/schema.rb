d1d67e69144d88ad072f827bfb6721512e21589e
Nedim Sisic <nsisic1@etf.unsa.ba>
2022-05-27 17:38:34 +0200

    Manually created database; Add User

--- db/schema.rb
+++ db/schema.rb
@@ -10,5 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 0) do
+ActiveRecord::Schema[7.0].define(version: 2022_05_27_152437) do
+  create_table "users", id: false, force: :cascade do |t|
+    t.integer "ignore", default: 0
+    t.string "email", default: "", null: false
+    t.string "encrypted_password", default: "", null: false
+    t.string "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.index ["email"], name: "index_users_on_email", unique: true
+    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
+  end
+
 end
