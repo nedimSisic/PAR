998bd22ce8b61521bb7fa8cbbe4f626a677cd46a
Nedim Sisic <nsisic1@etf.unsa.ba>
2022-06-13 02:09:55 +0200

    Added navbar

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  get 'pages/home'
   devise_for :users
   root 'base_pages#home'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
