63f720f400d9d4bf3a8dc21dca8ef354f4374324
Nedim Sisic <nsisic1@etf.unsa.ba>
2022-06-14 00:08:53 +0200

    Add Fullname to User; fix route bug

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,10 @@ Rails.application.routes.draw do
   root 'base_pages#home'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
+  devise_scope :user do
+    get '/users/sign_out' => 'devise/sessions#destroy'
+  end
+
   # Defines the root path route ("/")
   # root "articles#index"
 end
