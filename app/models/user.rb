df520d8b19cba5e46ab6cab232e342a6705fa23c
AlbertKhaid <107373951+AlbertKhaid@users.noreply.github.com>
2022-06-14 00:22:37 +0200

    Added full name validation

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,8 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
+  validates :fullname, presence: true, length:  {maximum: 60}
+
 	# has_secure_password
 	# had to comment above out so devise works: Rails NoMethodError: undefined method `password_digest=
 
